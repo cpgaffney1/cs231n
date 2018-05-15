c0ad321983c8c46e16ede2ec3dcb00962797fbd6
Colin Gaffney <cgaffney@stanford.edu>
2018-05-15 16:28:30 -0700

    reducing lr

--- experiment.py
+++ experiment.py
@@ -33,7 +33,7 @@ def train(args):
         model_folder = 'models/' + args.folder + '/'
     else:
         config = Config(word_index, embedding_matrix, imagenet_weights=True, trainable_convnet_layers=20,
-                    n_classes=1000)
+                    n_classes=1000, lr=0.0001)
         model = build_model(config)
         if os.path.exists('models/' + args.name):
             print('A folder with that name already exists.')
