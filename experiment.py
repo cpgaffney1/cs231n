cbf051e4e1d711deb5cbafe27db1984597971830
Colin Gaffney <cgaffney@stanford.edu>
2018-05-15 11:15:40 -0700

    reduced trainable layers

--- experiment.py
+++ experiment.py
@@ -32,7 +32,7 @@ def train(args):
         config, model = load_model(args.folder)
         model_folder = 'models/' + args.folder + '/'
     else:
-        config = Config(word_index, embedding_matrix, imagenet_weights=True, trainable_convnet_layers=20,
+        config = Config(word_index, embedding_matrix, imagenet_weights=True, trainable_convnet_layers=10,
                     n_classes=100)
         model = build_model(config)
         if os.path.exists('models/' + args.name):
