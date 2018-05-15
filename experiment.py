e00736a0c64b1de53167cebc3eed431d540f8956
Colin Gaffney <cgaffney@stanford.edu>
2018-05-14 22:42:46 -0700

    added argparser

--- experiment.py
+++ experiment.py
@@ -35,7 +35,7 @@ def train(args):
     embedding_matrix = util.load_embedding_matrix(word_index)
 
     config = Config(word_index, embedding_matrix, imagenet_weights=True, trainable_convnet_layers=20,
-                    n_classes=500)
+                    n_classes=100)
     model = build_model(config)
     train_model(model, config, numeric_data, text_data, model_folder)
 
