d83b8e79ebc68195c4370b77a3b91918f1eb9835
Colin Gaffney <cgaffney@stanford.edu>
2018-05-30 22:17:24 -0700

    changed bucketing

--- util.py
+++ util.py
@@ -76,7 +76,7 @@ def crop(image, shape=(299, 299), random=False):
 
 
 def buckets(x, bins):
-    y = np.digitize(x, bins, right=True)
+    y = np.digitize(x, bins, right=False)
     return y
 
 def get_bins(prices, num=1000):
