8b975d63ab77b2b43dde239dfc3eaf62f6b5aa1a
Colin Gaffney <cgaffney@stanford.edu>
2018-05-30 19:46:18 -0700

    reduced lr again

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ def build_model(config):
 
     #running fc
     #x = BatchNormalization()(numeric_inputs)
-    x = Dense(512, activation='relu', kernel_regularizer=regularizers.l2(config.reg_weight))(x)
+    x = Dense(512, activation='relu', kernel_regularizer=regularizers.l2(config.reg_weight))(numer_inputs)
     x = Dense(256, activation='relu', kernel_regularizer=regularizers.l2(config.reg_weight))(x)
     x = Dropout(config.drop_prob)(x)
     x = Dense(128, activation='relu', kernel_regularizer=regularizers.l2(config.reg_weight))(x)
