d7a679e5266b0ba74563b29b6c4390b422019a22
Colin Gaffney <cgaffney@stanford.edu>
2018-06-01 15:48:44 -0700

    custom loss function

--- model.py
+++ model.py
@@ -104,6 +104,7 @@ def build_model(config):
     def custom_loss(y_true, y_pred):
         main_loss = losses.sparse_categorical_crossentropy(y_true, y_pred)
         pred_indices = K.argmax(y_pred, axis=-1)
+        pred_indices = K.cast(pred_indices, dtype='float32')
         distance_penalty = K.constant(1.0, dtype='float32') / K.abs(pred_indices - K.constant(config.n_classes / 2.0, dtype='float32'))
         return main_loss + config.distance_weight * distance_penalty
 
